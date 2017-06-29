64cd363d4643b6a2b65acc4baddf82a4f56e92ac
Giuseppe <volumeetgrafica@MacBook-Pro-di-Volumeet.local>
2017-06-29 17:26:24 +0200

    element test

--- horse/src/App.js
+++ horse/src/App.js
@@ -1,18 +1,384 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/base.css';
+import './css/box.css';
+import './css/flat-ui-slider.css';
+import './css/font-awesome.css';
+import './css/landings.css';
+import './css/landings_layouts.css';
+import './css/layout.css';
+import './css/main.css';
+import './css/menu.css';
+import './css/pixicon.css';
+import './css/skeleton.css';
+
+import './js/jquery-1.7.2.min.js';
+import './js/jquery.easing.1.3.js';
+import './js/jquery.common.min.js';
+import './js/custom.js';
+import './js/easing.1.3.js';
+import './assets/smoothscroll.min.js';
+import './assets/appear.min.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="pixfort_corporate_2">
+
+              <div className="header_style">
+                  <div className="container">
+                      <div className="sixteen columns header_area">
+                          <img src="src/images/2_corporate/logo.png" alt=""/>
+
+                              <a href="#" className="yt_button"></a>
+                              <a href="#" className="twitter_button"></a>
+                              <a href="#" className="facebook_button"></a>
+
+                              <div className="htext_style">Stay connected</div>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+          <div className="pixfort_corporate_2">
+
+              <div className="page_style">
+                  <div className="container">
+                      <div className="sixteen columns context_style">
+
+                          <div className="title_style">
+                              CORPORATE LANDING PAGE
+                          </div>
+                          <div className="subtitle_style">
+                              More than 10 unique unbounce templates in one bundle isn't that awesome with a lot of features and great design brought to you by PixFort.
+                          </div>
+
+                          <div className="contact_style">
+                              <div className="contact_btn"> <a href="#">Contact us now</a> </div>
+                          </div>
+
+
+                          <div className="note_st">*Note: Bundle ends in two weeks from now.</div>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+
+          <div className="pixfort_corporate_2">
+
+              <div className="adv_st">
+                  <div className="container">
+                      <div className="sixteen columns">
+
+                          <div className="four columns  alpha">
+                              <img src="src/images/2_corporate/1.png" alt=""/>
+                                  <div className="head_style">Calculator</div>
+                                  <div className="c_style">
+                                      The edges may be directed or undirected For example if the vertice represent people party.
+                                  </div>
+                          </div>
+
+                          <div className="four columns  ">
+                              <img src="src/images/2_corporate/2.png" alt=""/>
+                                  <div className="head_style">Daily Mails</div>
+                                  <div className="c_style">
+                                      The edges may be directed or undirected For example if the vertice represent people party.
+                                  </div>
+                          </div>
+
+                          <div className="four columns  ">
+                              <img src="src/images/2_corporate/3.png" alt=""/>
+                                  <div className="head_style">Coffe Cup</div>
+                                  <div className="c_style">
+                                      The edges may be directed or undirected For example if the vertice represent people party.
+                                  </div>
+                          </div>
+
+                          <div className="four columns  omega">
+                              <img src="src/images/2_corporate/4.png" alt=""/>
+                                  <div className="head_style">Connection</div>
+                                  <div className="c_style">
+                                      The edges may be directed or undirected For example if the vertice represent people party.
+                                  </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+
+
+          <div className="pixfort_corporate_2">
+
+              <div className="amazing_style">
+                  <div className="container">
+                      <div className="sixteen columns">
+                          <div className="gstyle">
+                              <div className="t1_style">The most amazing thing is here</div>
+                              <div className="t2_style">Our service is astonishingly thin and light.</div>
+                              <div className="t3_style">Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud.</div>
+                          </div>
+                          <span> <img src="src/images/2_corporate/promo-image.png" className="pub_st" alt=""/></span>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+
+          <div className="pixfort_corporate_2">
+
+              <div className="adv_style">
+                  <div className="container">
+                      <div className="sixteen columns">
+                          <div className="L1_style">Great people trusted our services</div>
+                          <div className="L2_style">great words from great people</div>
+
+                          <div className="four columns just_st alpha">
+                              <div className="h_style">+950</div>
+                              <div className="cc_style">Themes and Templates Sales</div>
+                          </div>
+
+                          <div className="four columns just_st ">
+                              <div className="h_style">+150</div>
+                              <div className="cc_style">Followers love and trust us</div>
+                          </div>
+
+                          <div className="four columns just_st ">
+                              <div className="h_style">+85</div>
+                              <div className="cc_style">Items selling on themeforest</div>
+                          </div>
+
+                          <div className="four columns just_st omega">
+                              <div className="h_style">+6K</div>
+                              <div className="cc_style">Working hours this year wow</div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+
+
+          <div className="pixfort_corporate_2">
+
+              <div className="amazing_style">
+                  <div className="container">
+                      <div className="sixteen columns">
+                          <div className="gstyle">
+                              <div className="t1_style">Our Services Prices and Plans</div>
+                              <div className="t2_style">Our service is totaly thin and light.</div>
+                              <div className="t3_style">Lorem ipsum dolor sit amet consectetur  enim ad minim veniam quis nostrud.</div>
+                          </div>
+
+
+                          <div className="one-third column  alpha">
+                              <div className="th1_style">
+                                  <div className="a_bloc1_style">
+                                      <p className="a1_style">Free</p>
+                                      <p className="a2_style">Get All The Freebies</p>
+                                  </div>
+
+                                  <br/>
+                                      <p className="a3_style">5 Projects</p>
+                                      <p className="a3_style">1 GB Storage</p>
+                                      <p className="a3_style">No Domain</p>
+                                      <p className="a3_style">1 User</p>
+                                      <p className="a3_style"><span className="fsc1">0$</span><sup>/m</sup> </p>
+
+                                      <div className="a_bloc3_style">
+                                          <div className="subscribe_st">
+
+                                              <span className="subscribe_a"> <a href="#" className="slow_fade">Subscribe</a> </span>
+                                          </div>
+                                      </div>
+                              </div>
+                          </div>
+
+                          <div className="one-third column ">
+                              <div className="th2_style">
+                                  <div className="b_bloc1_style">
+                                      <p className="b1_style">Personal</p>
+                                      <p className="b2_style">Get All The Features</p>
+                                  </div>
+
+                                  <br/>
+                                      <p className="b3_style">25 Projects</p>
+                                      <p className="b3_style">50 GB Storage</p>
+                                      <p className="b3_style">with 1 Domain</p>
+                                      <p className="b3_style">10 Users</p>
+                                      <p className="b3_style"><span className="fsc2">9,99$</span><sup>/m</sup> </p>
+
+                                      <div className="b_bloc3_style">
+                                          <div className="subscribe_st">
+
+                                              <span className="subscribe_b"> <a href="#" className="slow_fade">Subscribe</a> </span>
+                                          </div>
+                                      </div>
+                              </div>
+                          </div>
+
+                          <div className="one-third column   omega">
+                              <div className="th1_style">
+                                  <div className="a_bloc1_style">
+                                      <p className="a1_style">Company</p>
+                                      <p className="a2_style">Get All The Features</p>
+                                  </div>
+
+                                  <br/>
+                                      <p className="a3_style">100 Projects</p>
+                                      <p className="a3_style">250 GB Storage</p>
+                                      <p className="a3_style">Unlimited Domains</p>
+                                      <p className="a3_style">50 Users</p>
+                                      <p className="a3_style"><span className="fsc1">70$</span><sup>/m</sup> </p>
+
+                                      <div className="a_bloc3_style">
+                                          <div className="subscribe_st">
+                                              <span className="subscribe_a"> <a href="#" className="slow_fade">Subscribe</a> </span>
+                                          </div>
+                                      </div>
+                              </div>
+                          </div>
+
+
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+
+
+          <div className="pixfort_corporate_2">
+
+              <div className="logos_style">
+                  <div className="container">
+                      <div className="sixteen columns">
+                          <div className="LL1_style">Companies We Work With</div>
+                          <div className="LL2_style">great words from great people</div>
+                          <span> <img src="src/images/2_corporate/logos.png" className="logos" alt=""/> </span>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+
+
+          <div className="pixfort_corporate_2">
+
+              <div className="awesome_style">
+                  <div className="container">
+                      <div className="sixteen columns">
+
+                          <h1 className="h_awesome"> What our awesome clients say </h1>
+
+                          <p className="txt_awesome">
+                              Great service with fast and reliable support The design work and detail put into themes are great.
+                          </p>
+
+                          <div className="via_st">
+                              <img src="src/images/2_corporate/stars.original.png" alt=""/>
+                                  <div className="top_3">via Unbounce.com</div>
+                          </div>
+
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+
+          <div className="pixfort_corporate_2">
+
+              <div className="contact_section">
+                  <div className="container">
+                      <div className="sixteen columns">
+                          <div className="eight columns  alpha">
+                              <div className="headtext_style">See our office</div>
+                              <p className="subtext_style">Our service is totaly thin and light.</p>
+                              <div className="plan">
+                                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83998.91163207508!2d2.3470599!3d48.85885894999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis%2C+France!5e0!3m2!1sen!2s!4v1408382253934"></iframe>
+                              </div>
+                          </div>
+
+
+                          <div className="eight columns omega">
+                              <div className="contact_zone">
+                                  <div className="headtext_style"><span className="editContent"><span className="pix_text">Contact us now</span></span></div>
+                                  <p className="subtext_style editContent">Our service is totaly thin and light.</p>
+                                  <div className="contact_st">
+                                      <form id="contact_form" pix-confirm="hidden_pix_2">
+                                          <div id="result"></div>
+                                          <input type="text" name="name" id="name" placeholder="Your Full Name" className="pix_text"/>
+                                              <input type="text"  name="company" id="company" placeholder="Your Company" className="pix_text"/>
+                                                  <input type="email" name="email" id="email" placeholder="Enter Your Email" className="pix_text"/>
+                                                      <textarea  rows="5" name="message" className="text_area pix_text" placeholder="Write your Message Here"></textarea>
+                                                      <button type="submit" className="subscribe_btn submit_btn_landing pix_text" id="subscribe_btn_2">
+                                                          <span className="editContent">Send it right away</span>
+                                                      </button>
+                                      </form>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+          <div id="hidden_pix_2" className="confirm_page confirm_page_2">
+              <div className="pixfort_corporate_2 big_padding">
+                  <div className="confirm_header"><span className="editContent"><span className="pix_text">WOW, You Are AWESOME</span></span></div>
+                  <div className="confirm_text">
+                      <span className="editContent"><span className="pix_text">Your form has been submitted and we will contact you as soon as possible.</span></span>
+                  </div>
+                  <div className="center_text padding_bottom_60">
+                      <ul className="bottom-icons confirm_icons center_text big_title pix_inline_block">
+                          <li><a className="pi pixicon-facebook6 white" href="#fakelink"></a></li>
+                          <li><a className="pi pixicon-twitter6 white" href="#fakelink"></a></li>
+                          <li><a className="pi pixicon-googleplus7 white" href="#fakelink"></a></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+
+          <div className="pixfort_corporate_2">
+
+              <div  className="foot_st">
+                  <div className="container ">
+                      <div className="seven columns alpha ">
+            <span className="rights_st"> All rights reserved Copyright &copy; 2014 FLATPACK by
+                    <span className="pixfort_st">PixFort</span>
+            </span>
+                      </div>
+
+
+                      <div className="nine columns omega ">
+                          <div className="socbuttons">
+
+                              <div className="soc_icons">
+                                  <ul className="bottom-icons">
+                                      <li><a className="pi pixicon-facebook2 normal_gray" href="#fakelink"></a></li>
+                                      <li><a className="pi pixicon-twitter2 normal_gray" href="#fakelink"></a></li>
+                                      <li><a className="pi pixicon-instagram normal_gray" href="#fakelink"></a></li>
+                                  </ul>
+                              </div>
+                              <div className="likes_st">Your likes &amp; share makes us happy!</div>
+
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
       </div>
     );
   }
